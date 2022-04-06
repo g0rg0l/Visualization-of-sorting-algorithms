d2f7522ef06d3e18d162893e4678c001cde607a1
Dmitry <70606894+g0rg0l@users.noreply.github.com>
2022-04-06 19:44:55 +0300

    06.04.2022
    1. Текст под столбцами гистограммы, который показывает "высоту столбца"
    2. Изменено количество столбцов с 50 до 40, чтобы было больше места для текста под столбцами
    3. Добавлены вспомогательные функции для обработки текста при его печати

--- main.c
+++ main.c
@@ -1,25 +1,22 @@
 #include <windows.h>
-#include "main.h"
 #include <gl/gl.h>
+#include <stb_easy_font.h>
 
-#define MAX_COUNT 50
+#include "main.h"
 
-int windowWidth = 1920;
-int windowHeight = 1080;
-
-float alpha = 0; // rotation coefficient
+#define MAX_COUNT 40
+#define WINDOW_HEIGHT 1080
+#define WINDOW_WIDTH 1920
 
 int numbers[MAX_COUNT]; // array to be sorted
 
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-    float usableHeight = (windowHeight * 9) / 20; // 486 px
-    float pxPerOne = usableHeight / MAX_COUNT; // 9.72 px
+    float pxPerOne =  WINDOW_HEIGHT / 2 / MAX_COUNT; // 9.72 px
 
     srand(time(NULL));
     arrayInit(); // initial filling of the array by numbers: [1, 50]
-
 
     WNDCLASSEX wcex;
     HWND hwnd;
@@ -28,7 +25,6 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     MSG msg;
     BOOL bQuit = FALSE;
 
-    /* register window class */
     wcex.cbSize = sizeof(WNDCLASSEX);
     wcex.style = CS_OWNDC;
     wcex.lpfnWndProc = WindowProc;
@@ -42,19 +38,18 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     wcex.lpszClassName = "GLSample";
     wcex.hIconSm = LoadIcon(NULL, IDI_APPLICATION);;
 
-
     if (!RegisterClassEx(&wcex))
         return 0;
 
     // Create main window
     hwnd = CreateWindowEx(0,
                           "GLSample",
-                          "Visualization of sorting algorithms",
-                          WS_OVERLAPPEDWINDOW,
+                          "Visualization of sorting algorithms", // Title
+                          WS_POPUP,
                           CW_USEDEFAULT,
                           CW_USEDEFAULT,
-                          windowWidth, // Main window width
-                          windowHeight, // Main window height
+                          WINDOW_WIDTH, // Main window width
+                          WINDOW_HEIGHT + 1, // Main window height
                           NULL,
                           NULL,
                           hInstance,
@@ -62,16 +57,12 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
     ShowWindow(hwnd, nCmdShow);
 
-    /* enable OpenGL for the window */
     EnableOpenGL(hwnd, &hDC, &hRC);
 
-    /* program main loop */
     while (!bQuit)
     {
-        /* check for messages */
         if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
         {
-            /* handle or dispatch messages */
             if (msg.message == WM_QUIT)
             {
                 bQuit = TRUE;
@@ -84,24 +75,20 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         }
         else
         {
-            // Painting by OpenGL
-
             glClearColor(0.88, 0.8, 0.74, 0);
             glClear(GL_COLOR_BUFFER_BIT);
 
             glPushMatrix();
-            // glRotatef(alpha, 1.0f, 1.0f, 1.0f);
 
             glBegin(GL_QUADS);
             glColor3f(0.04, 0.47, 0.44);
 
-                int i;
-                for (i = 0; i < MAX_COUNT; i++)
+                for (int i = 0; i < MAX_COUNT; i++)
                 {
-                    glVertex2f(-0.8 + i * 0.032, -0.90);
-                    glVertex2f(-0.8 + (i + 1) * 0.032, -0.90);
-                    glVertex2f(-0.8 + (i + 1) * 0.032, - (1 - (numbers[i] * pxPerOne / usableHeight)) + 0.1);
-                    glVertex2f(-0.8 + i * 0.032, - (1 - (numbers[i] * pxPerOne / usableHeight)) + 0.1);
+                    glVertex2f(-0.8 + i * 0.041, -0.9);
+                    glVertex2f(-0.8 + (i + 1) * 0.041, -0.9);
+                    glVertex2f(-0.8 + (i + 1) * 0.041, -0.9 + pxPerOne * numbers[i] / 540);
+                    glVertex2f(-0.8 + i * 0.041, -0.9 + pxPerOne * numbers[i] / 540);
                 }
 
             glEnd();
@@ -109,33 +96,51 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
             glBegin(GL_LINE_LOOP);
             glColor3f(0.92, 0.80, 0.51);
 
-                for (i = 0; i < MAX_COUNT; i++)
+                for (int i = 0; i < MAX_COUNT; i++)
                 {
-                    glVertex2f(-0.8 + i * 0.032, -0.90);
-                    glVertex2f(-0.8 + i * 0.032, - (1 - (numbers[i] * pxPerOne / usableHeight)) + 0.1);
-                    glVertex2f(-0.8 + (i + 1) * 0.032, - (1 - (numbers[i] * pxPerOne / usableHeight)) + 0.1);
-                    glVertex2f(-0.8 + (i + 1) * 0.032, -0.90);
+                    glVertex2f(-0.8 + i * 0.041, -0.9);
+                    glVertex2f(-0.8 + i * 0.041, -0.9 + pxPerOne * numbers[i] / 540);
+                    glVertex2f(-0.8 + (i + 1) * 0.041, -0.9 + pxPerOne * numbers[i] / 540);
+                    glVertex2f(-0.8 + (i + 1) * 0.041, -0.9);
                 }
 
             glEnd();
 
+
+            glColor3f(0.92, 0.80, 0.51);
+            glScalef(0.0027, -0.0027, 1);
+
+            int step = 0;
+
+            for (int i = 0; i < MAX_COUNT; i++)
+            {
+                if (numbers[i] < 10)
+                {
+                    step += 3;
+                    print_string(-294 + step, 340, toArray(numbers[i]), 0, 0, 0);
+                    step += 13;
+                }
+                else
+                {
+                    step -= 1;
+                    print_string(-294 + step, 340, toArray(numbers[i]), 0, 0, 0);
+                    step += 16;
+                }
+            }
+
             glPopMatrix();
             SwapBuffers(hDC);
-
-            // alpha += 0;
 
             Sleep (1);
         }
     }
 
-    /* shutdown OpenGL */
     DisableOpenGL(hwnd, hDC, hRC);
-
-    /* destroy the window explicitly */
     DestroyWindow(hwnd);
 
     return msg.wParam;
 }
+
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
@@ -166,16 +171,15 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
     return 0;
 }
 
+
 void EnableOpenGL(HWND hwnd, HDC* hDC, HGLRC* hRC)
 {
     PIXELFORMATDESCRIPTOR pfd;
 
     int iFormat;
 
-    /* get the device context (DC) */
     *hDC = GetDC(hwnd);
 
-    /* set the pixel format for the DC */
     ZeroMemory(&pfd, sizeof(pfd));
 
     pfd.nSize = sizeof(pfd);
@@ -191,11 +195,11 @@ void EnableOpenGL(HWND hwnd, HDC* hDC, HGLRC* hRC)
 
     SetPixelFormat(*hDC, iFormat, &pfd);
 
-    /* create and enable the render context (RC) */
     *hRC = wglCreateContext(*hDC);
 
     wglMakeCurrent(*hDC, *hRC);
 }
+
 
 void DisableOpenGL (HWND hwnd, HDC hDC, HGLRC hRC)
 {
@@ -204,10 +208,39 @@ void DisableOpenGL (HWND hwnd, HDC hDC, HGLRC hRC)
     ReleaseDC(hwnd, hDC);
 }
 
+
 void arrayInit()
 {
     for (int i = 0; i < MAX_COUNT; i++)
     {
-        numbers[i] = 1 + rand() % 50; // [1; 50]
+        numbers[i] = 1 + rand() % 40; // [1; 40]
     }
+}
+
+
+void print_string(float x, float y, char *text, float r, float g, float b)
+{
+  static char buffer[99999]; // ~500 chars
+  int num_quads;
+
+  num_quads = stb_easy_font_print(x, y, text, NULL, buffer, sizeof(buffer));
+
+  glColor3f(r,g,b);
+  glEnableClientState(GL_VERTEX_ARRAY);
+  glVertexPointer(2, GL_FLOAT, 16, buffer);
+  glDrawArrays(GL_QUADS, 0, num_quads*4);
+  glDisableClientState(GL_VERTEX_ARRAY);
+}
+
+
+char * toArray(int number)
+{
+    int n = log10(number) + 1;
+    int i;
+    char *numberArray = calloc(n, sizeof(char));
+    for (i = n-1; i >= 0; --i, number /= 10)
+    {
+        numberArray[i] = (number % 10) + '0';
+    }
+    return numberArray;
 }
--- main.h
+++ main.h
@@ -8,3 +8,9 @@ void DisableOpenGL(HWND, HDC, HGLRC);
 
 
 void arrayInit();
+
+
+void print_string(float x, float y, char *text, float r, float g, float b);
+
+
+char * toArray(int number);
